d335b247d8496fa41142e485b8d1d095ee8fc142
elhadis <ahmad23soho@gmail.com>
2021-03-20 00:26:26 +0200

    newcommit

--- main.js
+++ main.js
@@ -11,14 +11,42 @@ function newItem() {
     let usersList = document.createElement("li");
     let userInput = document.getElementById("userInput").value;
     let t = document.createTextNode(userInput);
-    let close = document.createElement("span");
+
+     ////// create close button
+    let close = document.createElement("button");
+
+    /////create div to  checkbox and close button
+    let newDiv = document.createElement("div");
+
+     ////// create checkbox   
+     let x = document.createElement("INPUT");
+     x.className = " w-75 d-none"
+     x.setAttribute("type", "checkbox");
+
+   
 
 
+    //// userList stylle
     usersList.className = "m-2 d-flex justify-content-between "
 
+    //// close button stylle
+    close.className = "btn-close w-25 m-e-12"
 
-    close.className = "btn-close"
-    usersList.append(close)
+     //// div stylle
+     newDiv.className = "d-inline-flex"
+     newDiv.append(x)
+    
+
+     
+
+
+     newDiv.append(close)
+    
+      usersList.append(newDiv)
+
+
+   
+    
 
     usersList.append(t)
 
@@ -30,6 +58,14 @@ function newItem() {
         document.getElementById("first").append(usersList)
     }
     document.getElementById("userInput").value = "";
+
+    usersList.addEventListener("click",()=>{
+
+      x.className = "w-100m-t-10 "
+      x.checked =true;
+    })
+
+   
 
     close.addEventListener("click", () => {
 
